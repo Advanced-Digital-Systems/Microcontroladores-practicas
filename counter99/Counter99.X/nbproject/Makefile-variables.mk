eac00974224c9f4e31e67f91384750101ce0f1b6
EdisonAltamirano <A00825234@itesm.mx>
2020-09-07 17:46:51 -0500

    Changes

---
+++ counter99/Counter99.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Counter99.X.production.null
+CND_ARTIFACT_PATH_default=dist/default/production/Counter99.X.production.null
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=counter99.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/counter99.x.tar
