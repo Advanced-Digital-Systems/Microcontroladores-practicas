b29b464731b4fddca9adfeedaf3a57a7e4ee0ffc
Elturriza <ghturriza@gmail.com>
2020-10-14 21:37:45 -0500

    Correccion y escribir comentarios

--- Proyecto1/Proyecto1.X/main.c
+++ Proyecto1/Proyecto1.X/main.c
@@ -47,9 +47,9 @@
  */
 
 void display(int);
-void display(int num){
-    // Asigna los valores del PortD de acuerdo a los numeros enteros
-    switch(num){
+void display(int disp){
+    // Asigna valores al display 
+    switch(disp){
         case 0:
             PORTA = 0b01111110;
             break;
@@ -108,62 +108,55 @@ void main(void)
     PORTD = 0xF;
     while (1)
     {
-        // Si un botón ha sido presionado, se inicia un scaneo cambiando el valor del puerto B para detectar la fila que se presionó
-        if(button_pressed){
-            // Si el puerto B sigue encendido cuando solo está activa la fila 0, esa es la fila presionada
+        // Este es el la funcion que hace reconocer que boton es presionado
+        if(button){
+            // Si el puerto B sigue encendido la fila presionada es la activa
             PORTD = 0x1;
             if(PORTB){
                 row = 0;
             }
-            // Si el puerto B sigue encendido cuando solo está activa la fila 1, esa es la fila presionada
             PORTD = 0x2;
             if(PORTB){
                 row = 1;
             }
-            // Si el puerto B sigue encendido cuando solo está activa la fila 2, esa es la fila presionada
             PORTD = 0x4;
             if(PORTB){
                 row = 2;
             }
-            // Si el puerto B sigue encendido cuando solo está activa la fila 3, esa es la fila presionada
             PORTD = 0x8;
             if(PORTB){
                 row = 3;
             }
             PORTD = 0xF;
             
-            // Se checa el conteo de los botones presionados (máximo 4, porque la contraseña es de 4 caracteres) para comparar la entrada a la contraseña correcta al final
             // Por default la contraseña está correcta, pero esto puede cambiar a falso en cualquier etapa.
             count++; // Se incrementa el conteo, para darle seguimiento a la secuencia
             
-            // Si se presionó '#', se resetea el conteo y se empieza de nuevo la lectura de la contraseña
+            // Si se presiona # se resetea todo
             if (keypad[row][col] == '#') { 
                 count = 0;
-                correct_password = true;
+                correct = true;
             }
-            // La 1ra vez que se presiona el botón, se decide que la contraseña es incorrecta si no hay match con el 1er caracter
+            //Aqui se revisara cada digito para eso el count++ para revisar que se presionen los 4 numeros del codigo.
+            // Revisa cada caracter uno por uno empezando por el primero y si es incorrecto marca false antes de ir al siguiente caracter,
             if (count == 1) {
-                if(!(keypad[row][col] == password_first)) {
-                    correct_password = false;
+                if(!(keypad[row][col] == primer)) {
+                    correct = false;
                 }
-            // La 2da vez que se presiona el botón, se decide que la contraseña es incorrecta si no hay match con el 2do caracter
             } else if (count == 2) {
-                if(!(keypad[row][col] == password_second)) {
-                    correct_password = false;
+                if(!(keypad[row][col] == segundo)) {
+                    correct = false;
                 }
-            // La 3ra vez que se presiona el botón, se decide que la contraseña es incorrecta si no hay match con el 3er caracter
             } else if (count == 3) {
-                if(!(keypad[row][col] == password_third)) {
-                    correct_password = false;
+                if(!(keypad[row][col] == tercero)) {
+                    correct = false;
                 }
-            // La 4ta vez que se presiona el botón, se decide que la contraseña es incorrecta si no hay match con el 4to caracter
-            // Después, se checa si hasta este punto la contraseña es correcta o falsa (y se decide la salida del sistema)
             } else if (count == 4) {
-                if(!(keypad[row][col] == password_fourth)) {
-                    correct_password = false;
+                if(!(keypad[row][col] == cuarto)) {
+                    correct = false;
                 }
-                // Si la contraseña es correcta, se abre y se cierra el servo (caja fuerte)
-                if(correct_password == true) {
+                //Si la contraseña es correcta se muestra en el display que si lo es, suena el boozer y se enciende el led verde.
+                if(correct == true) {
                     PORTE = 0b0001;             // Activa el display de decenas
                     display(1);                    // Imprime decenas
                     IO_RC2_SetHigh();
@@ -172,7 +165,7 @@ void main(void)
                     IO_RC2_SetLow();
                     IO_RC6_SetLow();  
                     PORTE = 0b0000;
-                // Si la contraseña es incorrecta, se activa el buzzer (alarma) y se cambia el valor de correct_password a true para reiniciar la lectura
+                // Si la contraseña es incorrecta, se activa el buzzer (alarma) y se enciende el led rojo y el display mostrando error
                 } else {
                     PORTE = 0b0001;             // Activa el display de decenas
                     display(0);                    // Imprime decenas
@@ -183,11 +176,11 @@ void main(void)
                     IO_RC1_SetLow();            // Turn off Buzzer              
                     PORTE = 0b0000;
                     
-                    correct_password = true;
+                    correct = true;
                 }
                 count = 0;
             }
-            button_pressed = false;
+            button = false;
         }        
     }
 }
